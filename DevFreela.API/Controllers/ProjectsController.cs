e8039adb92add043588cf881b95f6bd1843e2c75
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2024-08-23 11:34:00 -0300

    Revert "Configurações"
    This reverts commit 41d77b14f4c14733c1c05c18c2561a3ed859985a.

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,19 +1,11 @@
 using DevFreela.API.DTOs;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class ProjectsController : ControllerBase
 {
-    private readonly FreelanceTotalCostConfig _config;
-    public ProjectsController(IOptions<FreelanceTotalCostConfig> options)
-    {
-        _config = options.Value;
-    }
-    
     [HttpGet]
     public IActionResult Get(string search)
     {
@@ -29,11 +21,6 @@ public class ProjectsController : ControllerBase
     [HttpPost]
     public IActionResult Post(CreateProjectDTO model)
     {
-        if (model.TotalCost < _config.Minimum || model.TotalCost > _config.Maximum)
-        {
-            return BadRequest("Numero fora dos limites.");
-        }
-        
         return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
     }
 
